d13c64f123a15e07871d81fd10fcec9806ef8e9b
Victoria Hale <vic.hale74@gmail.com>
2019-10-14 03:46:30 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,24 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/\b[aeiou]/i) ? true : false
+  
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/un\w+ing/i)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+if text.match(/^[A-Z].+\.$/) 
+  true
+else
+  false
+end
 end
 
 def valid_phone_number?(phone)
-
+phone.match(/^[(]?\d{3}[)]?[\s]?\d{3}[\s-]?\d{4}$/)
 end
